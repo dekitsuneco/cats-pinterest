ec24b6a2edee297986d160e6da4736aee35ba285
dekitsuneco <47333988+dekitsuneco@users.noreply.github.com>
2022-05-28 13:39:36 +0500

    Create basic version of a CatCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
+import { CatsCard } from './components/CatsCard/CatsCard';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <CatsCard />
+      <CatsCard />
+    </div>
+  );
 }
 
 export default App;
